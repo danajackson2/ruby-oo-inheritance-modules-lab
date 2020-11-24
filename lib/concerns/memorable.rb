31d7152ad719f4418f59732ea7938db95ce2cb06
github username <github email address>
2020-11-24 14:39:51 -0800

    yaya

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,22 +1,16 @@
 module Memorable
     module ClassMethods
-    
         def count
             self.all.count
         end
-
         def reset_all
             self.all.clear
-        end
-
-        def find_by_name(name)
-            self.all.detect{|a| a.name == name}
-        end
+        end 
     end
 
     module InstanceMethods
-        def to_param
-            name.downcase.gsub(' ', '-')
-        end
+        def initialize
+            self.class.all << self
+        end 
     end
 end
