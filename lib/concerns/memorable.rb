e86b95c3e90f7f2417c741aab4ccd578e68320c9
github username <github email address>
2020-11-24 14:21:24 -0800

    all red to all green

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,22 @@
+module Memorable
+    module ClassMethods
+    
+        def count
+            self.all.count
+        end
+
+        def reset_all
+            self.all.clear
+        end
+
+        def find_by_name(name)
+            self.all.detect{|a| a.name == name}
+        end
+    end
+
+    module InstanceMethods
+        def to_param
+            name.downcase.gsub(' ', '-')
+        end
+    end
+end
